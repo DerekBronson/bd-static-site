6f80c0426fa1043fc9db36308120468d2ffdf4f4
Derek - Personal <derek-personal@Dereks-14-MB-Pro.local>
2026-05-20 13:13:54 -0700

    - Added an index markdown page - Added a template html page to use when generating html - Updated main.py to fully convert md to html and save to html file - Updated main.sh to create a local webserver to check work

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 import os
 import shutil
 
+from markdown_conversion import markdown_to_html_node
+
 
 def main():
     generate_content("./static", "./public", True)
+    generate_page("./content/index.md", "./template.html", "./public/index.html")
 
 
 def generate_content(src, dst, del_existing=False):
@@ -19,6 +22,39 @@ def generate_content(src, dst, del_existing=False):
         else:
             # Found a folder, dig deeper
             generate_content(os.path.join(src, i), os.path.join(dst, i))
+
+
+def extract_title(markdown):
+    title = ""
+    with open(markdown, "r") as file:
+        for line in file:
+            if line.startswith("# "):
+                title = line[2:].strip()
+    return title
+
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating a page from {from_path} to {dest_path} using {template_path}")
+
+    with open(from_path, "r") as file:
+        from_content = file.read()
+
+    with open(template_path, "r") as file:
+        template_content = file.read()
+
+    # Generate html nodes and html file
+    html_node = markdown_to_html_node(from_content)
+    html = ""
+    for child in html_node.children:
+        html += child.to_html()
+
+    title = extract_title(from_path)
+
+    dest_content = template_content.replace("{{ Title }}", title)
+    dest_content = dest_content.replace("{{ Content }}", html)
+
+    with open(dest_path, "w") as dest_file:
+        dest_file.write(dest_content)
 
 
 if __name__ == "__main__":
